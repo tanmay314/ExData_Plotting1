e8fde7efc81db63800bc0d442f2ad0c5843e7d04
tanmay314 <tanmay314@gmail.com>
2020-06-04 05:58:31 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -6,11 +6,11 @@ setwd("C:/Post_School/Coursera/Data_Science/exploratory-data-analysis/exdata_dat
 
 allData <- read.csv("C:/Post_School/Coursera/Data_Science/exploratory-data-analysis/exdata_data_household_power_consumption/household_power_consumption.txt", sep=";", header=TRUE)
 
-allData[,1] <- dmy(allData[,1])
+plotData <- filter(allData, (dmy(Date) == "2007-02-01") | (dmy(Date) == "2007-02-02"))
 
-plotData <- filter(allData, (Date == "2007-01-01") | (Date == "2007-02-02"))
+plotData[,1] <- dmy(plotData[,1])
 
-plotData[,"Global_active_power"] <- as.numeric(plotData[,"Global_active_power"])
+plotData[,"Global_active_power"] <- as.numeric(as.character((plotData[,"Global_active_power"])))
 
 hist(plotData$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
 
